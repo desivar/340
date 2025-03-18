2c0a5738631d0e387d9feea009c5d45544df5a1e
desivar <jillian_desire@yahoo.es>
2025-03-18 12:31:28 -0600

    add  constpath, add file view/index.ejs

--- routes/static.js
+++ routes/static.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const path = require('path');
 // Static Routes
 // Set up "public" folder / subfolders for static files
 router.use(express.static("public"));
