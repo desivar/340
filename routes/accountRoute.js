bb73e27363b70cb10603418bd471d2333293ca68
desivar <jillian_desire@yahoo.es>
2025-03-24 10:02:00 -0600

    modifications

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,57 +1,85 @@
-// Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const accountController = require("../controllers/accountController")
-const { handleErrors, checkLogin } = require("../utilities")
-const regValidate = require('../utilities/account-validation')
+// Needed Resources
+const express = require("express");
+const router = new express.Router();
+const accountController = require("../controllers/accountController");
+const { handleErrors, checkLogin } = require("../utilities");
+const regValidate = require('../utilities/account-validation');
+const Util = require('../utilities'); // Import your utilities
 
 // Route to build default account view
-router.get("/", checkLogin, handleErrors(accountController.buildAccount));
+router.get("/", checkLogin, handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await accountController.buildAccount(req, res, nav);
+}));
 
 // Route to build account register view
-router.get("/register", handleErrors(accountController.buildRegister));
+router.get("/register", handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await accountController.buildRegister(req, res, nav);
+}));
 
 // Process the registration data
 router.post(
   "/register",
   regValidate.registationRules(),
   regValidate.checkRegData,
-  handleErrors(accountController.registerAccount)
-)
+  handleErrors(async (req, res) => {
+    const nav = await Util.getNav(req, res);
+    await accountController.registerAccount(req, res, nav);
+  })
+);
 
 // Route to build account login view
-router.get("/login", handleErrors(accountController.buildLogin));
+router.get("/login", handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await accountController.buildLogin(req, res, nav);
+}));
 
 // Process the login attempt
 router.post(
   "/login",
   regValidate.loginRules(),
   regValidate.checkLoginData,
-  handleErrors(accountController.accountLogin)
-)
+  handleErrors(async (req, res) => {
+    const nav = await Util.getNav(req, res);
+    await accountController.accountLogin(req, res, nav);
+  })
+);
 
-// Route to build account login view
-router.get("/edit/:account_id", handleErrors(accountController.buildEditAccount));
+// Route to build account edit view
+router.get("/edit/:account_id", handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await accountController.buildEditAccount(req, res, nav);
+}));
 
 // Process the updated account information
 router.post(
   "/accountupdate",
   regValidate.updateAccountRules(),
   regValidate.checkEditAccountData,
-  handleErrors(accountController.editAccountInfo)
-)
+  handleErrors(async (req, res) => {
+    const nav = await Util.getNav(req, res);
+    await accountController.editAccountInfo(req, res, nav);
+  })
+);
 
 // Process the account password change
 router.post(
   "/changepassword",
   regValidate.changePasswordRules(),
   regValidate.checkEditAccountData,
-  handleErrors(accountController.editAccountPassword)
-)
+  handleErrors(async (req, res) => {
+    const nav = await Util.getNav(req, res);
+    await accountController.editAccountPassword(req, res, nav);
+  })
+);
 
 router.get(
   "/logout",
-  handleErrors(accountController.logoutAccount),
-)
+  handleErrors(async (req, res) => {
+    const nav = await Util.getNav(req, res);
+    await accountController.logoutAccount(req, res, nav);
+  })
+);
 
-module.exports = router
+module.exports = router;
--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -1,56 +1,87 @@
-// Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const messageController = require("../controllers/messageController")
-const { handleErrors } = require("../utilities")
-const validate = require("../utilities/message-validation")
+// Needed Resources
+const express = require("express");
+const router = new express.Router();
+const messageController = require("../controllers/messageController");
+const { handleErrors } = require("../utilities");
+const validate = require("../utilities/message-validation");
+const Util = require('../utilities'); // Import your utilities
 
 // INDEX
 // Route to build inbox/inbox view
-router.get("/", handleErrors(messageController.buildInbox));
+router.get("/", handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await messageController.buildInbox(req, res, nav);
+}));
 
 // ARCHIVED MESSAGES
 // Route to build inbox/archivedmessages view
-router.get("/archivedmessages", handleErrors(messageController.buildArchivedMessages));
+router.get("/archivedmessages", handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await messageController.buildArchivedMessages(req, res, nav);
+}));
 
 // SEND
 // Route to build inbox/sendmessage view
-router.get("/send", handleErrors(messageController.buildSendMessage));
+router.get("/send", handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await messageController.buildSendMessage(req, res, nav);
+}));
 
 // Route to build inbox/sendmessage view
 router.post(
-  "/send", 
+  "/send",
   validate.messageRules(),
   validate.checkMessageData,
-  handleErrors(messageController.sendMessage)
+  handleErrors(async (req, res) => {
+    const nav = await Util.getNav(req, res);
+    await messageController.sendMessage(req, res, nav);
+  })
 );
 
 // VIEW
 // Route to build inbox/viewmessage view
-router.get("/view/:message_id", handleErrors(messageController.buildViewMessage));
+router.get("/view/:message_id", handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await messageController.buildViewMessage(req, res, nav);
+}));
 
 // REPLY
 // Route to build account reply message view
-router.get("/reply/:message_id", handleErrors(messageController.buildReplyMessage));
+router.get("/reply/:message_id", handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await messageController.buildReplyMessage(req, res, nav);
+}));
 
 // Route to build account reply message view
 router.post(
   "/reply",
   validate.replyRules(),
   validate.checkReplyData,
-  handleErrors(messageController.replyMessage)
+  handleErrors(async (req, res) => {
+    const nav = await Util.getNav(req, res);
+    await messageController.replyMessage(req, res, nav);
+  })
 );
 
 // READ
 // Route to set message_read = true
-router.get("/read/:message_id", handleErrors(messageController.readMessage));
+router.get("/read/:message_id", handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await messageController.readMessage(req, res, nav);
+}));
 
 // ARCHIVE
 // Route to set message_archived = true
-router.get("/archive/:message_id", handleErrors(messageController.archiveMessage));
+router.get("/archive/:message_id", handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await messageController.archiveMessage(req, res, nav);
+}));
 
 // DELETE
 // Route to delete a message
-router.get("/delete/:message_id", handleErrors(messageController.deleteMessage));
+router.get("/delete/:message_id", handleErrors(async (req, res) => {
+  const nav = await Util.getNav(req, res);
+  await messageController.deleteMessage(req, res, nav);
+}));
 
-module.exports = router
+module.exports = router;
